64be0a3a7fd7d0899038fc93d35537b16a5269f5
Diego <munizmsb001@gmail.com>
2025-04-09 11:26:23 -0300

    exercicio08 enviado

--- exercicio07.py
+++ exercicio07.py
@@ -4,7 +4,6 @@ for a in range(10):
     n = int(input("Digite um numero :"))
 if n>=10 and n<=20:
     dentro = dentro + 1
-    print("numeros de dentro")
 else:
     fora= fora+1
     print(dentro, fora)
---
+++ exercicio08.py
@@ -0,0 +1,7 @@
+soma= 0
+for a in range(6):
+    n = int(input("Digite um numero :"))
+    soma=soma+n
+media=soma/5
+print(media)
+
